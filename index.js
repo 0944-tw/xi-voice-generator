631bbb17c8e6f3938b2c3c938bd6fc949f985021
zeroninefourfour <zwec181818@proton.me>
2024-11-25 18:06:02 +0800

    Code Fix

--- index.js
+++ index.js
@@ -1,95 +1,92 @@
-var pinyin = require("chinese-to-pinyin")
-var express = require("express")
-var app = express()
-var wav = require("wav-concat")
+(async () => {
+  var pinyin = require("chinese-to-pinyin");
+  var express = require("express");
+  var app = express();
+  var wav = require("wav-concat");
 
-app.get("/generate",async (req,res) => {
+  var fs = require("fs");
+
+  const voices = fs.readdirSync("./voicebank");
+  if (!(await fs.existsSync("./temp")) ){
+    await fs.mkdirSync("./temp");
+  }
+  app.get("/generate", async (req, res) => {
     // set csrf header
-    res.header("Access-Control-Allow-Origin","*")
+    res.header("Access-Control-Allow-Origin", "*");
 
     // get "name" parameter from request
-    var name = req.query.name
-    var removeTone = req.query.removeTone
+    var name = req.query.name;
+    var removeTone = req.query.removeTone;
     var options = {
-        removeTone: false,
-        toneToNumber: true,
-        noToneWhenNoFiles: false,
-    }
-    if (!name){
-        return res.send("缺失要產生的語詞")
+      removeTone: false,
+      toneToNumber: true,
+      noToneWhenNoFiles: true,
+    };
+    if (!name) {
+      return res.send("缺失要產生的語詞");
     }
     if (removeTone) {
-        console.log("Tone Remove")
-        options.removeTone = true
-        options.toneToNumber = false
+      console.log("Tone Remove");
+      options.removeTone = true;
+      options.toneToNumber = false;
     }
     if (req.query.noToneWhenNoFiles) {
-        console.log("No Tone When No Files")
-        options.noToneWhenNoFiles = true
+      console.log("No Tone When No Files");
+      options.noToneWhenNoFiles = true;
     }
     // generate pinyin
-    var pinyinName = pinyin(name,options)
-    console.log(pinyinName)
-    var splited = pinyinName.split(" ")
-    var voicefiles = [
+    var pinyinName = pinyin(name, options);
+    console.log(pinyinName);
+    var splited = pinyinName.split(" ");
+    var voicefiles = [];
 
-    ]
-
-   
-    var missingFiles = []
+    var missingFiles = [];
     // check file exists
-    for (let i = 0; i < splited.length;i++){
-        var file = splited[i] + ".wav"
-        
-        var fs = require("fs")
-        if (fs.existsSync("./voicebank/" + file)){
-            voicefiles.push("./voicebank/" + file)
+    for (let i = 0; i < splited.length; i++) {
+      var file = splited[i] + ".wav";
+      if (voices.includes(file)) {
+        voicefiles.push("./voicebank/" + file);
+      } else {
+        // remove number
+        var noNumber = splited[i].replace(/\d/g, "");
+        file = noNumber + ".wav";
+        if (voices.includes(file)) {
+          voicefiles.push("./voicebank/" + file);
         } else {
-            missingFiles.push(splited[i])
-          if (!options.noToneWhenNoFiles) {
-            let replacedTextName = "./voicebank/" + splited[i].replace(/[1-5]/g,"") + ".wav"
-            if (fs.existsSync("./voicebank/" + splited[i].replace(/[1-5]/g,"") + ".wav")){
-                console.log("Replace " + file + " with " + replacedTextName)
-                voicefiles.push(replacedTextName)
-                
-            }
-            continue;
-          }
-          console.log(file + " not exists")
-
+          missingFiles.push(splited[i]);
         }
-    } 
-    // Combine voice files
-    if (!voicefiles.length){
-     return res.send("Error: No voice files found")
+      }
     }
-    var date = new Date()
-    var time = date.getTime()
+    // Combine voice files
+    if (!voicefiles.length) {
+      return res.send("Error: No voice files found");
+    }
+    var date = new Date();
+    var time = date.getTime();
 
-    var fileName = `${time}.mp3`
-    let wavs = await wav(voicefiles).concat("./temp/" + fileName)
-    wavs.on("error",(err) => {
-        return res.send("Error: " + err)
-    })
-    wavs.on("end",(output) => {
-        // send json with buffer
-        
-        res.json({
-            filePath: `./vocals/${fileName}`,
-            missingFiles: missingFiles
-        })
-    })
+    var fileName = `${time}.mp3`;
+    let wavs = await wav(voicefiles).concat("./temp/" + fileName);
+    wavs.on("error", (err) => {
+      return res.send("Error: " + err);
+    });
+    wavs.on("end", (output) => {
+      // send json with buffer
+
+      res.json({
+        filePath: `./vocals/${fileName}`,
+        missingFiles: missingFiles,
+      });
+    });
     setTimeout(() => {
-     // Delete File
-        fs.unlink("./temp/" + fileName,(err) => {
-            if (err){
-                console.log(err)
-            }
-        })
-    },15 * 1000)
-
-
-})
-app.use("/vocals",express.static("temp"))
-app.use("/",express.static("public"))
-app.listen(3000)
+      // Delete File
+      fs.unlink("./temp/" + fileName, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }, 15 * 1000);
+  });
+  app.use("/vocals", express.static("temp"));
+  app.use("/", express.static("public"));
+  app.listen(3000);
+})();
